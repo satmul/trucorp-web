bb196d0fafdb70ed10bb3432d0a843fa88cc88bb
Bryan <56126330+satmul@users.noreply.github.com>
2021-02-16 06:28:10 -0500

    Update index.php

--- Web/page/index.php
+++ Web/page/index.php
@@ -10,13 +10,13 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT COUNT(nama) as jumlah user FROM users";
+$sql = "SELECT COUNT(nama) as jumlah FROM users";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
 
   while($row = mysqli_fetch_assoc($result)) {
-    echo "Jumlah User: " . $row["jumlah user"]. "<br>";
+    echo "Jumlah User: " . $row["jumlah"]. "<br>";
   }
 } else {
   echo "Data kosong";
